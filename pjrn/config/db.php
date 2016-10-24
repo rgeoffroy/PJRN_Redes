21bd49ce5a78a1cae84e58523cebd1e8da2f6f37
Roberto Geoffroy <rgeoffroy@gmail.com>
2016-10-24 09:10:24 -0300

    Modificado el Formulario Alta de Telefonos

--- pjrn/config/db.php
+++ pjrn/config/db.php
@@ -5,6 +5,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=172.16.100.106;dbname=telefonos',
     'username' => 'root',
-    'password' => 'GqH5o3',
+    'password' => '',
     'charset' => 'utf8',
 ];
--- pjrn/views/telefono/_form.php
+++ pjrn/views/telefono/_form.php
@@ -1,7 +1,9 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\ArrayHelper;
 use yii\widgets\ActiveForm;
+
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Telefono */
@@ -27,8 +29,8 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'patrimonio')->textInput(['maxlength' => true]) ?>
 
     <?php 
-	    $items = ArrayHelper::map(Central::find()->all(), 'id', 'ip');
-	    $form->field($model, 'telefono')->dropDownList($items)
+	    $items = ArrayHelper::map(\app\models\Central::find()->all(), 'id', 'ip');
+	    echo $form->field($model, 'central_id')->dropDownList($items)
 	?>
 
     <?= $form->field($model, 'habilitado')->textInput() ?>
@@ -40,3 +42,4 @@ use yii\widgets\ActiveForm;
     <?php ActiveForm::end(); ?>
 
 </div>
+
